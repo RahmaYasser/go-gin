5c5714907b531cc43c2927223f198db17714a909
RahmaYasser <rahma.y9079@gmail.com>
2022-02-14 16:54:59 +0200

    jwt auth middleware

--- cmd/server.go
+++ cmd/server.go
@@ -1,52 +1,74 @@
 package main
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/RahmaYasser/go-gin/controller"
-	"github.com/RahmaYasser/go-gin/middleware"
 	"github.com/RahmaYasser/go-gin/service"
 	"github.com/gin-gonic/gin"
-	gindump "github.com/tpkeeper/gin-dump"
-	"io"
-	"os"
 )
 
 var (
-	videoService    service.VideoService       = service.New()
+	videoService service.VideoService = service.New()
+	loginService service.LoginService = service.NewLoginService()
+	jwtService   service.JWTService   = service.NewJWTService()
+
 	videoController controller.VideoController = controller.New(videoService)
+	loginController controller.LoginController = controller.NewLoginController(loginService, jwtService)
 )
 
-func setLoggingOutput() {
-	file, _ := os.Create("log")
-	gin.DefaultWriter = io.MultiWriter(file, os.Stdout)
-}
 func main() {
-	setLoggingOutput()
-	server := gin.New()
-	server.Use(gin.Recovery(), middleware.Logger(), middleware.BasicAuth(), gindump.Dump())
 
-	server.Static("/css", "./template/css")
+	server := gin.New()
+
+	server.Use(gin.Recovery(), gin.Logger())
+
+	server.Static("/css", "./templates/css")
+
 	server.LoadHTMLGlob("templates/*.html")
 
-	apiRoutes := server.Group("/api")
-	{
-		apiRoutes.GET("/videos", func(context *gin.Context) {
-			context.JSON(200, videoController.FindAll())
-		})
-		apiRoutes.POST("/videos", func(context *gin.Context) {
-
-			err := videoController.Save(context)
-			if err != nil {
-				context.JSON(400, gin.H{"error": err.Error()})
-				return
-			}
-			context.JSON(200, gin.H{
-				"message": "ok",
+	// Login Endpoint: Authentication + Token creation
+	server.POST("/login", func(ctx *gin.Context) {
+		token := loginController.Login(ctx)
+		if token != "" {
+			ctx.JSON(http.StatusOK, gin.H{
+				"token": token,
 			})
+		} else {
+			ctx.JSON(http.StatusUnauthorized, nil)
+		}
+	})
+
+	// JWT Authorization Middleware applies to "/api" only.
+	apiRoutes := server.Group("/api", middlewares.AuthorizeJWT())
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
+
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!!"})
+			}
+
 		})
 	}
+
+	// The "/view" endpoints are public (no Authorization required)
 	viewRoutes := server.Group("/view")
 	{
 		viewRoutes.GET("/videos", videoController.ShowAll)
 	}
-	server.Run("localhost:8081")
+
+	// We can setup this env variable from the EB console
+	port := os.Getenv("PORT")
+	// Elastic Beanstalk forwards requests to port 5000
+	if port == "" {
+		port = "5000"
+	}
+	server.Run(":" + port)
 }
